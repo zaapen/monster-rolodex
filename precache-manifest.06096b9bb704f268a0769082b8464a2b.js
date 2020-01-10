4be2f15b4a70a0f59761d1a96869e493b44fcdf1
Danny Hua <zaapen@gmail.com>
2020-01-09 23:37:02 -0800

    Updates

---
+++ precache-manifest.06096b9bb704f268a0769082b8464a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbd72339a46e59663de34dc7acd6a04",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "da7b0bafe563a585eaf4",
+    "url": "/monster-rolodex/static/css/main.52b506ac.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da7b0bafe563a585eaf4",
+    "url": "/monster-rolodex/static/js/main.6406322f.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
